d5644cb65ce256bd096b4c07a47a1e36a7c63732
seven <seventheli@outlook.com>
2023-05-28 20:41:37 +0100

    ver 0.01 update dqn with er

--- dqn.py
+++ dqn.py
@@ -3,10 +3,9 @@ import json
 import mlflow
 import argparse
 import datetime
-import collections
 from os import path
 from dynaconf import Dynaconf
-from utils import init_ray, check_path, log_with_timeout, convert_np_arrays
+from utils import init_ray, check_path, logs_with_timeout, convert_np_arrays
 from ray.rllib.algorithms.dqn import DQN
 
 checkpoint_path = "./checkpoint/"
@@ -40,14 +39,26 @@ mlflow_run = mlflow.start_run(run_name="DQN" + datetime.datetime.now().strftime(
 
 # Set algorithm
 hyper_parameters = settings.dqn.hyper_parameters.to_dict()
+mlflow.log_params({key: hyper_parameters[key] for key in hyper_parameters.keys() if key not in ["replay_buffer_config"]})
+mlflow.log_params(hyper_parameters["replay_buffer_config"])
 algorithm = DQN(config=hyper_parameters, env=settings.dqn.env)
 
+keys_to_extract = {"episode_reward_max", "episode_reward_min", "episode_reward_mean"}
 for i in tqdm.tqdm(range(1, 10000)):
     result = algorithm.train()
+    # statistics
+    evaluation = result.get("evaluation", None)
+    sampler = result.get("sampler_results", None)
     if i == 10 or i % settings.log.log == 0:
-        learner_data = result['info']['learner']
-        if learner_data.get("time_usage", None) is not None:
-            log_with_timeout()
+        learner_data = result["info"].copy()
+        learner_data.pop("learner")
+        if learner_data is not None:
+            logs_with_timeout(learner_data)
+            _save = {key: sampler[key] for key in keys_to_extract if key in sampler}
+            logs_with_timeout(_save)
+    if evaluation is not None:
+        _save = {"eval_" + key: evaluation[key] for key in keys_to_extract if key in evaluation}
+        logs_with_timeout(_save)
     with open(path.join(log_path, str(i) + ".json"), "w") as f:
         result["config"] = None
         json.dump(convert_np_arrays(result), f)
