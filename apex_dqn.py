7b43d4dfd47dba5aa31165fbb6cd0e18842b3c83
seven <seventheli@outlook.com>
2023-06-26 17:08:25 +0100

    ver 0.03 update final_epsilon: 0.01

--- apex_dqn.py
+++ apex_dqn.py
@@ -84,6 +84,10 @@ log_path = path.join(settings.log.save_file, settings.apex.env)
 check_path(log_path)
 log_path = path.join(log_path, run_name)
 check_path(log_path)
+checkout_path = path.join(settings.log.save_checkout, settings.apex.env)
+check_path(checkout_path)
+checkout_path = path.join(checkout_path, run_name)
+check_path(checkout_path)
 
 # Run algorithms
 keys_to_extract = {"episode_reward_max", "episode_reward_min", "episode_reward_mean"}
