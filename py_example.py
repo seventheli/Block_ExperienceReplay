bfe0767bc7d624c226783eb107cfb41e527bf099
seven <seventheli@outlook.com>
2023-07-12 15:23:06 +0100

    ver 0.045.dev
    building Singularity.def

--- py_example.py
+++ py_example.py
@@ -1,3 +1,8 @@
 import torch
+import json
+from utils import check_path
 
-print(torch.cuda.is_available())
+check_path("/tmp/")
+with open ("/tmp/test.json", "w") as f:
+    json.dump({"test": torch.cuda.is_available()}, f)
+print(torch.tensor([123,]).cuda())
