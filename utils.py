e7803e85e0f7a2a1c86ce38f2b856be7ab3eaaa9
Jo.ZHOU <seventheli@outlook.com>
2023-07-22 11:49:04 +0100

    ver 0.08.dev update files

--- utils.py
+++ utils.py
@@ -12,7 +12,7 @@ def init_ray(ray_setting=None):
     if ray_setting is not None:
         with open(ray_setting, 'r') as file:
             settings = yaml.safe_load(file)
-        ray.init(**settings)
+        ray.init(**settings, _system_config={"maximum_gcs_destroyed_actor_cached_count": 50})
     else:
         ray.init()
 
