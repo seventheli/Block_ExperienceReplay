9e1c28c027ae84f699278612fb9482331fdfa7c9
Jo.ZHOU <seventheli@outlook.com>
2023-07-27 11:04:19 +0100

    ver 0.1.dev update files

---
+++ tasks/cxz46/singularity_dpber_Qbert_32.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Activate conda environment and run nvidia-smi
+. /opt/conda/etc/profile.d/conda.sh
+conda activate ber
+nvidia-smi
+
+# Define paths
+_path="/jmain02/home/J2AD006/jxb06/cxz46-jxb06/Block_ExperienceReplay/"
+_log="/jmain02/home/J2AD006/jxb06/cxz46-jxb06/logging/Qbert"
+_checkpoint="/jmain02/home/J2AD006/jxb06/cxz46-jxb06/checkpoints/Qbert"
+
+# Change directory
+cd $_path || exit
+
+# Run Python script
+python $_path/apex_dqn.py \
+    -S $_path/settings/apex_ddqn/Qbert.yml \
+    -R $SLURM_JOB_ID \
+    -L $_log \
+    -C $_checkpoint \
+    -SBZ 32
