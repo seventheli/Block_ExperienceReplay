675f504618d28fa8fce8307a345a095ca73a1924
Jo.ZHOU <seventheli@outlook.com>
2023-08-23 07:35:47 +0100

    ver 0.5 upload data hpc

--- tasks/conda/Test_apex.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-. /opt/conda/etc/profile.d/conda.sh
-conda activate hpc
-nvidia-smi
-_path=/jmain02/home/J2AD006/jxb06/cxz46-jxb06/Block_ExperienceReplay/
-cd $_path || exit
-python $_path/apex_dqn.py -S $_path/settings/apex_ddqn/BeamRider.yml -SBZ 32 -R $SLURM_JOB_ID
