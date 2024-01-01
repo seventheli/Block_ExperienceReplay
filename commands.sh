a8492955af45e2500881b6c731090e2f1a7bc924
seventheli <seventheli@outlook.com>
2024-01-01 11:05:32 +0800

    ver 0.26.dev update files for JADE

--- commands.sh
+++ commands.sh
@@ -3,4 +3,4 @@ sbatch --mem=90G \
        --cpus-per-task=6 \
        --time=4-10:00:00 \
        -p small \
-       /jmain02/home/J2AD006/jxb06/cxz46-jxb06/Block_ExperienceReplay/tasks/dpber_LavaCrossingS9N3.sh:
+       /jmain02/home/J2AD006/jxb06/cxz46-jxb06/Block_ExperienceReplay/tasks/dpber_LavaCrossingS9N3.sh
