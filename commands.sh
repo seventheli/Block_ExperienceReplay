23ef6cda9988fde5eae37dc736b2a42fd389d3d2
seventheli <seventheli@outlook.com>
2024-01-25 20:21:00 +0000

    update for JADE

--- commands.sh
+++ commands.sh
@@ -1,11 +1,11 @@
-
 sbatch --mem=160G \
        --gres=gpu:1 \
        --cpus-per-task=20 \
        --time=4-10:00:00 \
        -p small \
-       /jmain02/home/J2AD006/jxb06/cxz46-jxb06/Block_ExperienceReplay/tasks/dpber_MiniGrid-LavaCrossingS9N3.sh
+       /jmain02/home/J2AD006/jxb06/cxz46-jxb06/Block_ExperienceReplay/tasks/dper_MiniGrid-LavaCrossingS9N3.sh
 
+sleep 14400
 
 sbatch --mem=160G \
        --gres=gpu:1 \
@@ -20,6 +20,15 @@ sbatch --mem=160G \
        --cpus-per-task=20 \
        --time=4-10:00:00 \
        -p small \
+       /jmain02/home/J2AD006/jxb06/cxz46-jxb06/Block_ExperienceReplay/tasks/dper_MiniGrid-DistShift2.sh
+
+sleep 14400
+
+sbatch --mem=160G \
+       --gres=gpu:1 \
+       --cpus-per-task=20 \
+       --time=4-10:00:00 \
+       -p small \
        /jmain02/home/J2AD006/jxb06/cxz46-jxb06/Block_ExperienceReplay/tasks/dpber_MiniGrid-Empty-8x8.sh
 
 
@@ -28,5 +37,22 @@ sbatch --mem=160G \
        --cpus-per-task=20 \
        --time=4-10:00:00 \
        -p small \
+       /jmain02/home/J2AD006/jxb06/cxz46-jxb06/Block_ExperienceReplay/tasks/dper_MiniGrid-Empty-8x8.sh
+
+sleep 14400
+
+sbatch --mem=160G \
+       --gres=gpu:1 \
+       --cpus-per-task=20 \
+       --time=4-10:00:00 \
+       -p small \
        /jmain02/home/J2AD006/jxb06/cxz46-jxb06/Block_ExperienceReplay/tasks/dpber_MiniGrid-ObstructedMaze-1Dlhb.sh
+
+
+sbatch --mem=160G \
+       --gres=gpu:1 \
+       --cpus-per-task=20 \
+       --time=4-10:00:00 \
+       -p small \
+       /jmain02/home/J2AD006/jxb06/cxz46-jxb06/Block_ExperienceReplay/tasks/dper_MiniGrid-ObstructedMaze-1Dlhb.sh
 
