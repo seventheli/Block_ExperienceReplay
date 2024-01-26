c757b4d47f070e58ae6802da286a454640f6f346
seventheli <seventheli@outlook.com>
2024-01-26 16:49:21 +0000

    update for JADE

--- commands.sh
+++ commands.sh
@@ -3,6 +3,13 @@ sbatch --mem=160G \
        --cpus-per-task=20 \
        --time=4-10:00:00 \
        -p small \
+       /jmain02/home/J2AD006/jxb06/cxz46-jxb06/Block_ExperienceReplay/tasks/dpber_MiniGrid-LavaCrossingS9N3.sh
+
+sbatch --mem=160G \
+       --gres=gpu:1 \
+       --cpus-per-task=20 \
+       --time=4-10:00:00 \
+       -p small \
        /jmain02/home/J2AD006/jxb06/cxz46-jxb06/Block_ExperienceReplay/tasks/dper_MiniGrid-LavaCrossingS9N3.sh
 
 sleep 14400
